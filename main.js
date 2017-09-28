9aca33aac483ea9b7616e5913313c6ed785c36bd
florestankorp <florestankorp@gmail.com>
2017-09-28 13:05:21 +0200

    Working calculator

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-let totalExpenses = document.querySelector('#total-expenses')
-let incomeLotte = document.querySelector('#income-lotte')
-let incomeFlorestan = document.querySelector('#income-florestan')
-
 let resultLotte = document.querySelector('#result-lotte')
 let resultFlorestan = document.querySelector('#result-florestan')
 
 let button = document.querySelector('#calculate-button')
 
 let collectResult = () => {
+
+  let totalExpenses = document.querySelector('#total-expenses')
+  let incomeLotte = document.querySelector('#income-lotte')
+  let incomeFlorestan = document.querySelector('#income-florestan')
+
   let shares = {}
 
   incomeFlorestan = parseInt(incomeFlorestan.value)
@@ -17,10 +18,10 @@ let collectResult = () => {
   let totalIncome = incomeLotte + incomeFlorestan
 
   let shareFlorestan = incomeFlorestan / totalIncome * totalExpenses
-  shares.florestan = shareFlorestan
+  shares.florestan = shareFlorestan.toFixed(2)
 
   let shareLotte = incomeLotte / totalIncome * totalExpenses
-  shares.lotte = shareLotte
+  shares.lotte = shareLotte.toFixed(2)
 
   return shares
 }
@@ -31,4 +32,4 @@ let printResult = () => {
   resultLotte.innerHTML = shares.lotte
 }
 
-button.onclick = function () { printResult() }
+button.addEventListener('click', printResult)
