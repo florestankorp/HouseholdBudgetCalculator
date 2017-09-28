43a7e36a6bbc59b31c6a4f354b22287a1883ea3a
florestankorp <florestankorp@gmail.com>
2017-09-28 10:10:47 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let resultLotte = document.querySelector('.result-lotte')
+let resultFlorestan = document.querySelector('.result-florestan')
+
+resultLotte.innerHTML = '10000'
+resultFlorestan.innerHTML = '20000'
