f487b3c48a97c9ea127d6285d28f98277365020a
Florestan Korp <florestankorp@gmail.com>
2019-11-06 19:51:00 +0100

    added new fetch listener

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-const FILES_TO_CACHE = ["./index.html", "./main.js", "./style.css"];
+const FILES_TO_CACHE = ['./index.html', './main.js', './style.css'];
 
-const CACHE_NAME = "pwa-app-cache";
+const CACHE_NAME = 'pwa-app-cache';
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            console.log("[ServiceWorker] Pre-caching offline page");
+            console.log('[ServiceWorker] Pre-caching offline page');
             return cache.addAll(FILES_TO_CACHE);
         })
     );
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(keyList => {
             return Promise.all(
                 keyList.map(key => {
                     if (key !== FILES_TO_CACHE) {
-                        console.log("[ServiceWorker] Removing old cache", key);
+                        console.log('[ServiceWorker] Removing old cache', key);
                         return caches.delete(key);
                     }
                 })
@@ -26,16 +26,22 @@ self.addEventListener("activate", event => {
     );
 });
 
-self.addEventListener("fetch", event => {
-    if (event.request.mode !== "navigate") {
-        // Not a page navigation, bail.
-        return;
-    }
-    event.respondWith(
-        fetch(event.request).catch(() =>
-            caches.open(CACHE_NAME).then(cache => {
-                return cache.match("index.html");
-            })
-        )
-    );
+self.addEventListener('fetch', event => {
+
+    if (event.request.method != 'GET') return;
+
+    event.respondWith(async function () {
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(event.request);
+
+        if (cachedResponse) {
+            event.waitUntil(cache.add(event.request));
+
+            console.log('[Service Worker] Fetched resource ' + event.request.url);
+            return cachedResponse;
+        }
+
+        console.log('[Service Worker] Fetched resource ' + event.request.url);
+        return fetch(event.request);
+    }());
 });
