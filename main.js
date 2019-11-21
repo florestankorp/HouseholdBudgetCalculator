d2aa7bc3fb17d3eb84590aa54495e56618a053ac
“Florestan <florestankorp@gmail.com>
2019-11-21 21:23:49 +0100

    resolved conflicts

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const button = document.querySelector('#calculate-button');
 const resultThelma = document.querySelector('#result-thelma');
 const resultLouise = document.querySelector('#result-louise');
 
+function closeOverlay() {
+  const overlay = document.querySelector('.overlay');
+  const template = document.getElementById('calculator-template');
+
+  const calculatorTemplate = document.importNode(template.content, true);
+  overlay.replaceWith(calculatorTemplate);
+}
+
 function calculate() {
   let shares = {};
 
@@ -76,4 +84,13 @@ function showResults(shareLouise, shareThelma) {
  * what if total is greater than combined income?
  * new function: saving for shared goal; e.g. how much should each of them put aside for a trip p/m
  * how much money do do you have left to spend?
+ * append child node when result is calculated
+ * add transitions for loading screen and starting calculator
+  window.onload = function() {
+    const myInput = document.getElementById('myInput');
+    myInput.onpaste = function(e) {
+      e.preventDefault();
+    }
+  }
+
  */
